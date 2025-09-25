a01a48c374277338e2fb057c3f8c8712664826dc
otavio <otavio.olivindo@aluno.ce.gov.br>
2025-09-25 14:56:47 -0300

    Add files via upload

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+# Atividade 5 – Concatenação de Strings
+# Crie duas variáveis: nome e sobrenome.
+# Mostre o nome completo juntando as duas.
+nome = str(input("diga seu nome: "))
+sobrenome = str(input("diga seu sobrenome:"))
+print(nome, sobrenome)
